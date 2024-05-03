61c5c9320e527eb6cca1c985ebcf51f39be42b5b
hyowonzzang <163090718+hyowonzzang@users.noreply.github.com>
2024-05-03 14:13:20 +0900

    Update Practice05.sql
    2024-05-03

--- Practice05.sql
+++ Practice05.sql
@@ -10,9 +10,29 @@ select first_name || ' ' || last_name as "이름",
 select employee_id as "직원번호",
 first_name as "이름",
 salary as "급여",
-to_char(hire_date,'YYYY-MM-DD') as "입사일",
+to_char(hire_date,'YYYY-MM-DD day') as "입사일",
 substr(replace(phone_number,'.','-'),3) as "전화번호",department_id as "부서번호"
 from employees
 where (department_id,salary) in (select department_id, max(salary)
 from employees group by department_id)
 order by salary desc;
+
+-- 문제3. 너무 많이 출력됨 *검토하기*
+select manager_id, first_name,avg(salary), min(salary), max(salary)
+from employees
+where hire_date >= '2015/01/01'
+group by manager_id, first_name
+having avg(salary)>= 5000
+order by avg(salary) desc;
+
+-- 문제4.
+select e.employee_id, e.first_name, d.department_name, e.first_name
+from employees e
+join departments d on (e.department_id = d.department_id);
+
+-- 문제5.
+select e.employee_id, e.first_name || ' ' || e.last_name, d.department_name, e.salary, e.hire_date
+from employees e
+join departments d on(e.department_id = d.department_id)
+where hire_date >='2015/01/01'
+order by hire_date asc;
